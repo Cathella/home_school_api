ba6bf696a22d6a32b9fa87320ca961304afb4f7a
Cathella <ncathie214@gmail.com>
2022-03-15 08:56:11 +0300

    Generate user

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:3000"
+    resource "*", headers: :any, metods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+ 
+  allow do
+    origins "https://home-school-api.herokuapp.com"
+    resource "*", headers: :any, metods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+end
